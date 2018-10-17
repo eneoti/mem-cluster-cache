a3efcc9b8baa129211ebc3aae681fa21b776cfc5
eneoti <namddt@gmail.com>
2018-10-17 17:50:28 +0700

    debug

--- test/cache.js
+++ test/cache.js
@@ -1,8 +1,8 @@
 'use strict'
 var redis = require('../')
 var dockerTest= require('docker-testing');
-var docker=new dockerTest.DockerTest()
-
+var docker=new dockerTest.DockerTest();
+var cacheService;
 describe('redis', function () {
 	before(async function(){
 	  this.timeout(10000);
@@ -19,16 +19,35 @@ describe('redis', function () {
 	    this.timeout(5000);
 	   await(docker.StopContainer("redis-test"))
 	  });
-	describe('#connect', function () {
-	    it('should return an CacheService when connect is called ', function (done) {
-	     	var cacheService=new redis.CacheService({
+	describe('#cacheService', function () {
+	    it('should receive event connect when init CacheService ', function (done) {
+	     	cacheService=new redis.CacheService({
 				port: 6379,          
 				host: "localhost"
 			})
 			cacheService._db.on('connect',function () {
-				cacheService._db.disconnect()
 				done();
 			});
 	    })
+	    it('save object json into redis ', function (done) {
+	     	cacheService.saveObject("things","name",{name:"thing1", age:40}).then(function(){
+	     		cacheService.saveObject("things","name",{name:"thing2", age:30}).then(function(){
+	     			done();
+	     		})
+	     	}).catch(function(err){
+	     		cacheService._db.disconnect()
+	     	})
+	    })
+	    it('read object json should same with saveObject before ', function (done) {
+	     	cacheService.getObject("things","thing1").then(function(result){
+	     		if(result.name=="thing1"){
+	     			done();
+	     		}
+	     		cacheService._db.disconnect()
+	     	}).catch(function(err){
+	     		cacheService._db.disconnect()
+	     	})
+	    })
 	})
+
 })
